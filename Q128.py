00027bc5d8d4de6d6db733ccf24e827b916352e8
Bikael <mmacdoug02@gmail.com>
2026-09-21 19:52:14 -0400

    1st submission Q128

---
+++ Q128.py
@@ -0,0 +1,27 @@
+def longestConsecutive(nums: list[int]) -> int:
+        num_set = set(nums)
+        cur_seq = 1
+        lcs = 1
+
+        for num in nums:
+            while num + 1 in num_set:
+                cur_seq += 1
+                num += 1
+
+            if cur_seq >= lcs:
+                lcs = cur_seq
+            cur_seq = 1    
+
+            while num - 1 in num_set:
+                cur_seq += 1
+                num -= 1
+
+            if cur_seq >= lcs:
+                lcs = cur_seq
+            cur_seq = 1  
+            
+
+            
+        return lcs
+
+print(longestConsecutive([100,4,200,1,3,2]))
