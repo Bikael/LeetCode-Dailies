b17a5792d361fa91f353b338395c414154f50436
Bikael <mmacdoug02@gmail.com>
2026-09-22 22:29:53 -0400

    needed a lot of help from gpt but got the optimal way to do it

--- Q3.py
+++ Q3.py
@@ -1,22 +1,23 @@
 def lengthOfLongestSubstring( s: str) -> int:
-    index = 0
-    dupe_map = {}
     longest_sub_array = 0
+    dupe_map = {}
+    start = 0
+    end = 0
 
-    while index < len(s):
-        if s[index] not in dupe_map:
-            dupe_map[s[index]] = index
-        else:  
-            index = dupe_map[s[index]]
-            dupe_map = {}
-            dupe_map[s[index]] = index + 1
-            
-        if len(dupe_map) > longest_sub_array:
-            longest_sub_array = len(dupe_map)
+    while end < len(s):
 
-        index += 1
+        if s[end] not in dupe_map:
+            dupe_map[s[end]] = end
+        else:
+            start = max(start, dupe_map[s[end]] + 1)
+            dupe_map[s[end]] = end
+            
+        end += 1
 
-        
-            
-            
-lengthOfLongestSubstring("1R1T7")
+        if longest_sub_array < end - start:
+            longest_sub_array = end - start
+
+    print(longest_sub_array)
+    return longest_sub_array
+
+lengthOfLongestSubstring("eea")
