1d358f86368b30d5015ee7cb709527ac3bfd0bc2
Bikael <mmacdoug02@gmail.com>
2026-09-24 18:20:42 -0400

    initial submission fails on a

--- Q5.py
+++ Q5.py
@@ -1,2 +1,32 @@
+def getPalindromeSubstring(s: str, left : int, right : int) -> str:
+    offset = 0
+    while ((left - offset) >= 0 and (right + offset) < len(s)):
+        # print(f"right offset {right + offset}")
+        # print(f"left offset {left - offset}")
+        if s[left - offset] == s[right + offset]:
+            offset += 1
+        else:
+            return s[left - offset + 1 : right + offset]
+
+    return s[left - offset + 1 : right + offset]
+
+# print(getPalindromeSubstring("dabba", 2 , 3))
+
 def longestPalindrome( s: str) -> str:
-    pass
+
+    longestPalindrome = ""
+    left = 0
+    right = 0
+
+    while left != len(s) and right != len(s):
+        if left == right:
+            right += 1
+        else:
+            left += 1
+
+        palindrome_sub = getPalindromeSubstring(s,left,right)
+        if len(palindrome_sub) > len(longestPalindrome):
+            longestPalindrome = palindrome_sub
+    return longestPalindrome
+
+print(longestPalindrome("a"))
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-# file used for testing python behaviors
-for i in range(0,10,1):
-    print(i)
-    if i == 5:
-        i = 2
-    print(i)
+mr_str = "badad"
 
+print(mr_str[1:3])
